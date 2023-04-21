b5d0f54d5ca17e30603e190755a0f6cfc7042451
BuildTools <infuriatingx1@gmail.com>
2023-04-21 20:30:41 +0200

    created colors array for random colors

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 let svg_circle = document.querySelectorAll(".svg_circle"),
   svg_color1 = document.querySelectorAll(".svg_circle_c1"),
-  svg_color2 = document.querySelectorAll(".svg_circle_c2");
+  svg_color2 = document.querySelectorAll(".svg_circle_c2"),
+  colors = [
+    "#001220",
+    "#00CC99",
+    "#560090",
+    "#6600FF",
+    "#FA7268",
+    "#C62368",
+    "#140021",
+    "#FF760E",
+    "#038373",
+    "#001829",
+    "297EA6",
+    "#715DF2",
+    "#C62368",
+    "#F7CACA",
+    "#9900FF",
+  ];
 
 function randomize() {
   for (let i = 0; i < svg_circle.length; i++) {
@@ -9,8 +26,8 @@ function randomize() {
     svg_circle[i].style.transition = "all 12.5s ease-in-out";
   }
 
-  let randomColor1 = "#" + Math.floor(Math.random() * 16777215).toString(16),
-    randomColor2 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  let randomColor1 = colors[Math.floor(Math.random() * colors.length)];
+  let randomColor2 = colors[Math.floor(Math.random() * colors.length)];
 
   for (let i = 0; i < svg_color1.length; i++) {
     svg_color1[i].setAttribute("fill", randomColor1);
